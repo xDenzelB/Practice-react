d4020b8aefad62643ab8a00b07b5676f3e2694b5
Denzel <dbartolaba123@gmail.com>
2022-01-29 13:48:13 -0800

    created button values and rendered screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
+import Wrapper from './components/Wrapper';
+import Screen from './components/Screen';
+import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
 
-function App() {
+const btnValues = [
+  ['C', '+-', '%', '/'],
+  [7, 8, 9, 'X'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <input className='numbers'></input>
-        <button className='calculate'>Submit</button>
-      </div>
-    </div>
+    <Wrapper>
+      <Screen value='0' />
+      <ButtonBox>
+        <Button className=""
+          value='0'
+          onClick={() => {
+            console.log('Button clicked!');
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
   );
-}
-
+};
 export default App;
